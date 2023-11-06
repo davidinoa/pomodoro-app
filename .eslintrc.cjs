4b6888f1504d98b7aab3dd89994b3e2c8f9f979d
David Inoa <davidinoa@gmail.com>
2023-11-06 14:08:30 -0500

    Update ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,15 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
   plugins: ['react-refresh'],
   rules: {
+    'import/no-absolute-path': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
