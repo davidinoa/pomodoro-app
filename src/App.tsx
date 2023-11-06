f588b173c7198f0bb2d1ecdb165aa19946570557
David Inoa <davidinoa@gmail.com>
2023-11-06 15:16:10 -0500

    Setup design system theme with Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div>
+      <h1 className="font-sans font-bold text-sunset">This is Kumbh Sans</h1>
+      <h2 className="font-serif text-whisper">This is Roboto Slab</h2>
+      <h3 className="font-mono text-orchid">This is Space Mono</h3>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sunset: '#f87070',
+        glacier: '#70f3f8',
+        orchid: '#d881f8',
+        nimbus: '#d7e0ff',
+        eclipse: '#13213f',
+        blizzard: '#ffffff',
+        whisper: '#eff1fa',
+        obsidian: '#161932',
+      },
+      fontFamily: {
+        sans: ['"Kumbh Sans"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Roboto Slab"', ...defaultTheme.fontFamily.serif],
+        mono: ['"Space Mono"', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
